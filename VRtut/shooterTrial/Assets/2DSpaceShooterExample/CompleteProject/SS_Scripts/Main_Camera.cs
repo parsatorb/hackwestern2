06a2e90cd3a74d98831050f482111ad5cc244424
Parsa Torabian <parsa.torabian@glocalspace.com>
2015-11-28 13:37:08 -0500

    Parsa's code before David took over

---
+++ VRtut/shooterTrial/Assets/2DSpaceShooterExample/CompleteProject/SS_Scripts/Main_Camera.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Main_Camera : MonoBehaviour {
+	Transform target;
+	float smooth;
+	// Use this for initialization
+	void Start () {
+		smooth = 5;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position.z = target.position.z -distance;
+		transform.position.y = target.position.y;
+		transform.position.x = target.position.x;
+
+		transform.position.x = Mathf.Lerp(transform.position.x,target.position.x,Time.deltaTime*smooth);
+		transform.position.y = Mathf.Lerp(transform.position.y,target.position.y,Time.deltaTime*smooth);
+	}
+}
